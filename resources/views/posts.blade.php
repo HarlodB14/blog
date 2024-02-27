960f5f605bdd4921d2a12919d588e58de6782d28
Harlod Bombala <h.bombala@student.avans.nl>
2024-02-27 21:22:43 +0100

    removed titles from posts, they are dynamic from metadata now and added blade syntax

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,14 +8,14 @@
 <?php foreach ($posts as $post) : ?>
 <article>
     <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
+        <a href="/posts/<?= $post->slug; ?>">
+            {{ $post->title }}
         </a>
 
     </h1>
 
     <div>
-            <?= $post->excerpt; ?>
+        {{ $post->excerpt }}
     </div>
 </article>
 
