b25a7227a24db6b1c7ea63a036de6a1040db9ef5
Harlod Bombala <h.bombala@student.avans.nl>
2024-03-13 00:20:21 +0100

    made seeders and added eloquent relationship to user and post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p> work Post</p>',
+            'body' => '<p>Cras accumsan vel urna ac pulvinar. Suspendisse feugiat placerat viverra. Vivamus rutrum rhoncus rutrum. Praesent vehicula elit sem, a congue elit ullamcorper ac. Fusce in tincidunt dolor, eget auctor lectus. Aenean ullamcorper mi in justo maximus, eu pellentesque magna convallis. Nunc congue lectus id lacus aliquet, vitae sollicitudin ex auctor. Aenean non lacus vitae sem consectetur hendrerit. Pellentesque quis enim accumsan</p>',
+
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p> work Post</p>',
+            'body' => '<p>Cras accumsan vel urna ac pulvinar. Suspendisse feugiat placerat viverra. Vivamus rutrum rhoncus rutrum. Praesent vehicula elit sem, a congue elit ullamcorper ac. Fusce in tincidunt dolor, eget auctor lectus. Aenean ullamcorper mi in justo maximus, eu pellentesque magna convallis. Nunc congue lectus id lacus aliquet, vitae sollicitudin ex auctor. Aenean non lacus vitae sem consectetur hendrerit. Pellentesque quis enim accumsan</p>',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'slug' => 'my-personal-post',
+            'excerpt' => '<p> work Post</p>',
+            'body' => '<p>Cras accumsan vel urna ac pulvinar. Suspendisse feugiat placerat viverra. Vivamus rutrum rhoncus rutrum. Praesent vehicula elit sem, a congue elit ullamcorper ac. Fusce in tincidunt dolor, eget auctor lectus. Aenean ullamcorper mi in justo maximus, eu pellentesque magna convallis. Nunc congue lectus id lacus aliquet, vitae sollicitudin ex auctor. Aenean non lacus vitae sem consectetur hendrerit. Pellentesque quis enim accumsan</p>',
+        ]);
     }
 }
